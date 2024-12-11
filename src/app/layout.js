d9979edb5f1e6e516594a3c479da467805f30c9b
foy4748 <faisaljfcl@gmail.com>
2024-12-11 19:12:14 +0600

    Initial Project Setup and Structured a bit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import NavBar from "./components/NavBar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +23,15 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <NavBar />
+
+        <main className="min-h-[600px] place-content-center place-items-center">
+          {children}
+        </main>
+
+        <footer className="text-center bg-slate-600">
+          Awesome NextJS project
+        </footer>
       </body>
     </html>
   );
